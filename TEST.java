ee8f772d2c0c98a55c4553976f3d00d1d2667494
GuoChenyang <15814751+AlexGuo077@user.noreply.gitee.com>
2025-05-12 11:19:29 +0800

    Good Start

--- TEST.java
+++ TEST.java
@@ -1,6 +1,7 @@
 public class TEST {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("GoodMorning!");
     }
 }
 //这是一个测试 Let's fucking go!
