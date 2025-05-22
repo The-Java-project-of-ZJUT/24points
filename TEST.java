860fe86bbcc7cbb3a584e30f5c8af08f98fd3bdf
GuoChenyang <3450356104@qq.com>
2025-05-22 15:19:46 +0800

    Delete TEST.java

--- TEST.java
+++
@@ -1,7 +0,0 @@
-public class TEST {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("GoodMorning!");
-    }
-}
-//这是一个测试 Let's fucking go!
