9d2981a4d854ea4dd82225581d843fa87138b848
GuoChenyang <3450356104@qq.com>
2025-05-22 17:10:36 +0800

    Main文件的大致思路

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // 启动游戏界面
+       
+        // 在界面输入用户信息，进行登录、注册等操作后跳转模式选择
+
+        // 新建一个GameRoom进行游戏，GR中等待用户点击不同模式按钮，根据不同模式进入不同GameMood
+        GameRoom gameRoom = new GameRoom();
+        
+        // GR可以一直循环直到玩家退出游戏，每次循环后更新玩家分数
+        
+        // 退出游戏后更新玩家分数并记录到排行榜并显示排行榜
+        
+        //...
+    }
+}
